75b704f4f877272475423f73f2244859a390aa06
贾延平 <yanping_jia@hotmail.com>
2016-03-09 20:19:44 +0800

    fix upate

--- opal/clearwater/cached_render.rb
+++ opal/clearwater/cached_render.rb
@@ -11,6 +11,7 @@ module Clearwater
           if(prev && prev.vnode && #{!should_render?(`prev`)}) {
             return prev.vnode;
           } else {
+            self.dom_node = self.dom_node || #{Clearwater::DOMReference.new}
             self.node = #{Component.sanitize_content(render)};
             self.node.properties['ref'] = #{@dom_node};
             return self.node;
@@ -24,11 +25,17 @@ module Clearwater
     end
 
     def update!
-      node = Component.sanitize_content(render)
-      diff = VirtualDOM.diff @node, node
-      tree = @dom_node.JS[:node].JS[:native]
-      VirtualDOM.patch tree, diff
-      @node = node
+      return if @will_render
+      @will_render = true
+      Bowser.window.animation_frame {
+#              Bowser.window.delay(0.001) {
+        node = Component.sanitize_content(render)
+        diff = VirtualDOM.diff @node, node
+        tree = @dom_node.JS[:node].JS[:native]
+        VirtualDOM.patch tree, diff
+        @node = node
+        @will_render = false
+      }
     end
   end
 end
